9789e4e246349ffd3ea18de590d90df784975f63
Imre Nagi <imre.nagi2812@gmail.com>
2020-06-21 17:49:44 +0700

    Implement isbinary search tree
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>
    
    Co-authored-by: Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>

--- tree/tree.go
+++ tree/tree.go
@@ -17,6 +17,43 @@ type Node struct {
 }
 
 // IsBinarySearchTree ...
+// Given a binary tree, determine if it is a valid binary search tree (BST).
+// Assume a BST is defined as follows:
+// * The left subtree of a node contains only nodes with keys less than the node's key.
+// * The right subtree of a node contains only nodes with keys greater than the node's key.
+// * Both the left and right subtrees must also be binary search trees.
+// Example 1:
+//  2
+// / \
+// 1   3
+//
+// Input: [2,1,3]
+// Output: true
+//
+// Example 2:
+//  5
+// / \
+// 1   4
+// 	  / \
+//   3   6
+// Input: [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4.
 func IsBinarySearchTree(node *Node) bool {
-	return false
+	min := -9999
+	max := 9999
+	return isBinarySearchTree(node, min, max)
+}
+
+func isBinarySearchTree(root *Node, min, max int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val <= min || root.Val >= max {
+		return false
+	}
+
+	return isBinarySearchTree(root.Left, min, root.Val) && isBinarySearchTree(root.Right, root.Val, max)
+
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -11,3 +11,76 @@ func TestDummy(t *testing.T) {
 	nd := NewNode(0, nil, nil)
 	assert.NotNil(t, nd)
 }
+
+func TestIsBinarySearchTree_Nil(t *testing.T) {
+	var node *Node
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_OneNode(t *testing.T) {
+	node := NewNode(5, nil, nil)
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Not_TwoLevel(t *testing.T) {
+	node := NewNode(1,
+		NewNode(2, nil, nil),
+		NewNode(3, nil, nil))
+	assert.False(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Not_TwoLevel_LeftNotExist(t *testing.T) {
+	node := NewNode(1,
+		nil,
+		NewNode(3, nil, nil))
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Not_TwoLevel_RightNotExist(t *testing.T) {
+	node := NewNode(2,
+		NewNode(1, nil, nil),
+		nil)
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Valid_TwoLevel(t *testing.T) {
+	node := NewNode(2,
+		NewNode(1, nil, nil),
+		NewNode(3, nil, nil))
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Invalid_IfDuplicate(t *testing.T) {
+	node := NewNode(2,
+		NewNode(2, nil, nil),
+		NewNode(3, nil, nil))
+	assert.False(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Invalid_IfDuplicate_OnRight(t *testing.T) {
+	node := NewNode(2,
+		NewNode(1, nil, nil),
+		NewNode(2, nil, nil))
+	assert.False(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Invalid_ThreeLevel(t *testing.T) {
+	node := NewNode(6,
+		NewNode(3, NewNode(5, nil, nil), NewNode(4, nil, nil)),
+		NewNode(8, nil, nil))
+	assert.False(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_valid_ThreeLevel(t *testing.T) {
+	node := NewNode(6,
+		NewNode(3, NewNode(2, nil, nil), NewNode(5, nil, nil)),
+		NewNode(8, nil, nil))
+	assert.True(t, IsBinarySearchTree(node))
+}
+
+func TestIsBinarySearchTree_Valid_ThreeLevel_Right(t *testing.T) {
+	node := NewNode(6,
+		NewNode(3, nil, nil),
+		NewNode(8, NewNode(7, nil, nil), NewNode(9, nil, nil)))
+	assert.True(t, IsBinarySearchTree(node))
+}
