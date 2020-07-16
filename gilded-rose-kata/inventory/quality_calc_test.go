7c22bfe090b463b066411ac0abd6965ed3fb5a75
Imre Nagi <imre.nagi2812@gmail.com>
2020-07-17 06:38:42 +0700

    Add gilded rose kata
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>
    
    Co-authored-by: Imre Nagi <imre.nagi@go-jek.com>

---
+++ gilded-rose-kata/inventory/quality_calc_test.go
@@ -0,0 +1,17 @@
+package inventory
+
+import (
+	"testing"
+)
+
+func Test_Foo(t *testing.T) {
+	var items = []*Item{
+		&Item{"foo", 0, 0},
+	}
+
+	UpdateQuality(items)
+
+	if items[0].name != "fixme" {
+		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
+	}
+}
